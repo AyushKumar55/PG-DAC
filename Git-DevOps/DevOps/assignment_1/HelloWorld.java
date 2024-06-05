0915f1ad4bf27540a6ae07a3362c612d36660ce1
Ayush Kumar <ayush9kumar9@gmail.com>
2024-06-05 22:21:01 +0530

    Update: Fixed Folder Structure

---
+++ Git-DevOps/DevOps/assignment_1/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+public static void main(String[] args){
+System.out.println("Hello World");
+System.out.println("This is from C-DAC ACTS ...");
+System.out.println("We are in DAC ...");
+}
+}
---
+++ Git-DevOps/Git/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("I am in Branch B-1");
+		System.out.println("Created Branch B-2 and changed from Branch B-2");
+	}
+}
